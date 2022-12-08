40a49c42ee5de9a22a57dbd8f749c56601c244f5
StoneT91 <timo.johannes.steiner@gmx.de>
2022-12-08 14:59:26 +0100

    update

--- smartHome.ino
+++ smartHome.ino
@@ -1,5 +1,6 @@
 #include "Alarm.h"
 #include "Sonar.h"
+//#include <Nextion.h>
 
 Sonar so;
 bool alarmOn = false;
@@ -9,6 +10,8 @@ int buzzer = 8;
 int pinActivate = 9;
 int pinLedGreen = 10;
 bool systemActiv = false;
+String cmd = "\"";
+char mySecondCharacter;
 
 // The setup() function runs once each time the micro-controller starts
 void setup() {
@@ -17,18 +20,47 @@ void setup() {
 	pinMode(panicButton, INPUT);
 	pinMode(pinLedGreen, OUTPUT);
 	Serial.begin(9600);
+	Serial3.begin(9600);
 }
 
 // Add the main program code into the continuous loop() function
 void loop() {
+	if (alarmOn != true) {
+		Serial3.print("t2.txt=" + cmd + "OFF" + cmd);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+	}
+
+	if (systemActiv != true) {
+		Serial3.print("t4.txt=" + cmd + "OFF" + cmd);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+	}
+
 	if (digitalRead(pinActivate)) {
 		delay(5000);
 		systemActiv = true;
 		digitalWrite(pinLedGreen, HIGH);
+
+		Serial3.print("t4.txt=" + cmd + "ON" + cmd);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+
+
+
 	}
 	if ((motionDetection() || digitalRead(panicButton)) && systemActiv) {
 		alarmOn = true;
 		digitalWrite(pinLedGreen, LOW);
+
+		Serial3.print("t2.txt=" + cmd + "ON" + cmd);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+		Serial3.write(0xff);
+
 		while (alarmOn) {
 			digitalWrite(buzzer, HIGH);
 			delay(1);
@@ -37,9 +69,24 @@ void loop() {
 			if (digitalRead(pinButton)) {
 				alarmOn = false;
 				systemActiv = false;
+				Serial3.print("t2.txt=" + cmd + "OFF" + cmd);
+				Serial3.write(0xff);
+				Serial3.write(0xff);
+				Serial3.write(0xff);
+				Serial3.print("t4.txt=" + cmd + "OFF" + cmd);
+				Serial3.write(0xff);
+				Serial3.write(0xff);
+				Serial3.write(0xff);
 			}
+
+			while (Serial3.available()) {
+				mySecondCharacter = Serial3.read();
+			}
+			Serial3.print("t4.txt=" + cmd + mySecondCharacter + cmd);
+			Serial3.write(0xff);
+			Serial3.write(0xff);
+			Serial3.write(0xff);
 		}
 	}
 	delay(50);
-
 }
