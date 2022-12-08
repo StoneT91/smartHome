77d73024218af63b835230d615b61be534418722
StoneT91 <timo.johannes.steiner@gmx.de>
2022-12-08 09:31:05 +0100

    funktioniert. LED rot grün

--- smartHome.ino
+++ smartHome.ino
@@ -3,33 +3,43 @@
 
 Sonar so;
 bool alarmOn = false;
-int buzzer = 8;
-int pinButton = 7;
 int panicButton = 6;
+int pinButton = 7;
+int buzzer = 8;
+int pinActivate = 9;
+int pinLedGreen = 10;
+bool systemActiv = false;
 
 // The setup() function runs once each time the micro-controller starts
 void setup() {
-    pinMode(buzzer, OUTPUT);
-    pinMode(pinButton, INPUT_PULLUP);
-    pinMode(panicButton, INPUT);
-    Serial.begin(9600);
+	pinMode(buzzer, OUTPUT);
+	pinMode(pinButton, INPUT_PULLUP);
+	pinMode(panicButton, INPUT);
+	pinMode(pinLedGreen, OUTPUT);
+	Serial.begin(9600);
 }
 
 // Add the main program code into the continuous loop() function
-void loop() { 
-    if (motionDetection() == true || digitalRead(panicButton)) { //
-        alarmOn = true;
-        while (alarmOn) {
-            digitalWrite(buzzer, HIGH); 
-            delay(1);
-            digitalWrite(buzzer, LOW);
-            delay(1);
+void loop() {
+	if (digitalRead(pinActivate)) {
+		delay(5000);
+		systemActiv = true;
+		digitalWrite(pinLedGreen, HIGH);
+	}
+	if ((motionDetection() || digitalRead(panicButton)) && systemActiv) {
+		alarmOn = true;
+		digitalWrite(pinLedGreen, LOW);
+		while (alarmOn) {
+			digitalWrite(buzzer, HIGH);
+			delay(1);
+			digitalWrite(buzzer, LOW);
+			delay(1);
+			if (digitalRead(pinButton)) {
+				alarmOn = false;
+				systemActiv = false;
+			}
+		}
+	}
+	delay(50);
 
-            if(digitalRead(pinButton)==HIGH){
-                alarmOn = false;
-            }
-        }
-    }
-    
-    delay(50);
 }
